ea34e01d675d2c90ef9f6d98014a410604fc35db
yash <amazonqa1029@gmail.com>
2020-11-02 09:20:24 +0530

    updateing notes

--- notes.py
+++ notes.py
@@ -2,28 +2,37 @@
 
 Tasks:
     0. genral:
-        1. model seems to haev higher accuarcy so exactly what to focus on??
-        2. testing method
-        3. other relaetd projects nptel curious(explaination + slides, summarization, combining vedios from differnt iits)
+        1. 
+        2. testing method:
+            1. both accuracy(optimizing metric) and fn, fp rate(satisfizing metric)
+        3. other relaetd projects nptel (explaination + slides, summarization, combining vedios from differnt iits)
         
     1. get labeled data:
         1. data:
-            1. source: nptel vedio, prof. site, google images
-            2. problem with only slides with no face or name eg. Computer Architecture Prof. Smruti Ranjan Sarangi
-            3. features:
+            1. source: 
+                1. nptel vedio
+                2. nptel site(not very helpfull)
+                3. onlinecourses.nptel.ac.in(standard xml format for prof name for course name)
+                4. prof. site(not standard xm format for photo)
+                5. google images
+            2. problem with only slides with no face or name:
+                1. eg. Computer Architecture Prof. Smruti Ranjan Sarangi
+                2. can mayne output only slides or something
+            3. features data should have:
                 1. unique names
+                2. good photos for face recog
             
         2. text:
             1. problems:
                 1. length of names eg 3 or 4 word names, abreviated names
                 2. honrifics get cut eg. Cloud Computing
-                3. how to recognise names from text in image??
                     
         3. image:
             1. image extraction in vedios:
                 1. problems: 
                     1. completely wrong image eg. prof. meenakshi d’souza
                     2. face in miniplayer not detected eg. dr. yogish sabharwal
+                    3. dark face detected not recognised
                     
                 2. if not found in first vedio how to change search parameters:
                     1. options: increase time duration, frames searched per sec for vedio
@@ -38,5 +47,3 @@ Tasks:
     2. face recognition:
     
 https://stackoverflow.com/questions/3490727/what-are-some-methods-to-analyze-image-brightness-using-python
-
-https://www.pyimagesearch.com/2017/04/10/detect-eyes-nose-lips-jaw-dlib-opencv-python/

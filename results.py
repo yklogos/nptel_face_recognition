ada5ec6bd3fb47a5a9a87629edc27362b776341f
yash <amazonqa1029@gmail.com>
2020-12-20 08:35:50 +0530

    updating notebooks

---
+++ results.py
@@ -0,0 +1,41 @@
+hand in labelled for prof. debashis ray
+2 profs taking 1 course Microscale Transport Processes in Chemical
+
+model errors:
+    1. most model errors happen due to some movement eg. looking towards or away from the blackboard, looking up-down
+    2. blurred face due to discontinuity in recording
+    3. most model errors due to side faces in prof. t.k.sengupta labelled faces
+    
+    
+vedio_path: D:\nptel_face_recognition\data\full_vedio\Aerospace Engineering\Foundation of Scientific Computing\vT40FWuvIB8.mp4
+151 - sharp movement towards the blackboard
+
+
+vedio_path: D:\nptel_face_recognition\data\full_vedio\Biotechnology\Introduction to Mechanobiology\Vnxy8BcCTUc.mp4
+true_name: prof. shamik sen
+for dr. p. gopinath 
+35 - blurred due to cut to writing
+108, 110 - looking down??
+169 -  blurred due to cut to writing
+
+for prof. t.k.sengupta
+100, 104, 105 - looking down??
+109 - blurred due to cut to writing
+168 - blurred due to cut to writing
+
+
+vedio_path: D:\nptel_face_recognition\data\full_vedio\Biotechnology\Introduction to Mechanobiology\Bu07aFJGeyw.mp4
+true_name: prof. shamik sen
+for prof. t.k.sengupta
+frames_list: [139, 140] - blurred due to cut to writing
+    
+    
+vedio_path: D:\nptel_face_recognition\data\full_vedio\Biotechnology\Biomedical nanotechnology\0md5JdAbj4U.mp4
+true_name: dr. p. gopinath 
+for prof. s. dasgupta
+frames_list: [38] - quick look towards the camera
+for prof. t.k.sengupta
+frames_list: [20, 26, 27 - quick look towards the blackoard
+              39, 40] - side view
+for prof: padma vankar
+frames_list: [19] - looks staright??

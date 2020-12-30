c52eba3270bb830864aae7705e5467b2a098a768
yash <amazonqa1029@gmail.com>
2020-12-30 21:34:55 +0530

    updating notebooks

--- utils.py
+++ utils.py
@@ -1,27 +1,10 @@
-import cv2
+import pickle
 
-def make_dict_from_lists(list1, list2, one2two = True):
-    if one2two:
-        final = {list1[i]:list2[i] for i in range(len(list1))}
-    else:
-        final = {list2[i]:list1[i] for i in range(len(list1))}
+def save_as_pickle(file_path, file):
+    with open(file_path, 'wb') as f:
+        pickle.dump(file, f)
 
-def make_rgb_bgr(img):
-    b,g,r = cv2.split(img)          
-    rgb_img = cv2.merge([r,g,b]) 
-    return rgb_img
-
-def read_bgr(path):
-    return make_rgb_bgr(cv2.imread("sample.jpg", -1))
-
-def imshowg(gray):
-    plt.imshow(gray, cmap='gray')
-    
-def plot_image_list(img_list, figsize=(12, 12), subplot_n_cols=2):
-    plt.figure(figsize=(12, 12))
-    for i,img in enumerate(img_list):
-        plt.subplot(int(len(img_list)/subplot_n_cols)+1, subplot_n_cols, i+1)
-        try:
-            plt.imshow(img)
-        except:
-            pass
+def load_from_pickle(file_path):
+    with open(file_path, 'rb') as f:
+        file = pickle.load(f)
+    return file
